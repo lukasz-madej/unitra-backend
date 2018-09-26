e9ff58004befa4fce38a500930d0cf03e1f66bb5
lukasz-madej <lukasz.madej@kreuzwerker.pl>
2018-09-26 21:52:57 +0200

    Adds categories

---
+++ categories/categories.controller.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const categoriesService = require('./categories.service');
+
+module.exports = router;
+
+router.put('/create', create);
+router.post('/update/:id', update);
+router.delete('/remove/:id', remove);
+router.get('/get/', getAll);
+
+function create(request, response, next) {
+  categoriesService.create(request.body)
+    .then(category => response.status(201).json(category))
+    .catch(error => next(error));
+}
+
+function update(request, response, next) {
+  categoriesService.update({ id: request.params.id, ...request.body })
+    .then(category => category ?
+      response.json(category) :
+      response.status(404).json({ message: 'Category to update not found' }))
+    .catch(error => next(error))
+}
+
+function remove(request, response, next) {
+  categoriesService.remove(request.params)
+    .then(categoriesService.removeCategoryRelations(request.params.id))
+    .then(category => response.status(200).json(category))
+    .catch(error => next(error));
+}
+
+function getAll(request, response, next) {
+  categoriesService.getAll()
+    .then(categories => response.status(201).json(categories))
+    .catch(error => next(error));
+}
---
+++ categories/categories.service.js
@@ -0,0 +1,44 @@
+const knex = require('knex') (require('../knexfile'));
+
+module.exports = {
+  create,
+  update,
+  remove,
+  getAll,
+  removeCategoryRelations
+};
+
+async function create({ name, description }) {
+  return await knex('categories')
+    .insert({
+      name,
+      description
+    });
+}
+
+async function update({ id, name, description }) {
+  return await knex('categories')
+    .where({ id })
+    .update({
+      name,
+      description
+    });
+}
+
+async function remove({ id }) {
+  return await knex('categories')
+    .where({ id })
+    .del();
+}
+
+async function getAll() {
+  return await knex('categories').select();
+}
+
+async function removeCategoryRelations(id) {
+  return await knex('equipment')
+    .where({ categoryId: id })
+    .update({
+      categoryId: 0
+    });
+}
