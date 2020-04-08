05dde9a778343adf0ebbd0f8c3b28118dbc15387
lukasz-madej <lukasz.madej@kreuzwerker.pl>
2020-04-08 20:37:46 +0200

    Uniform error handling

--- _helpers/error-handler.js
+++ _helpers/error-handler.js
@@ -2,12 +2,12 @@ module.exports = errorHandler;
 
 function errorHandler(error, request, response, next) {
   if (typeof error === 'string') {
-    return response.status(400).json({ message: error });
+    return response.status(400).json({ status: 400, message: error });
   }
 
   if (error.name === 'UnauthorizedError') {
-    return response.status(401).json({ message: 'Invalid Token' });
+    return response.status(401).json({ status: 401, message: 'Invalid Token' });
   }
 
-  return response.status(500).json({ message: error.message });
+  return response.status(500).json({ status: 500, message: error.message });
 }
